87b603504c8d1dd9d2bfe151a3108011c40ccafe
idanasulinStrech <idan@strech.io>
2022-03-16 14:28:17 +0200

    fix cors issue

--- routes/routes.go
+++ routes/routes.go
@@ -10,9 +10,9 @@ import (
 func InitializeHttpRoutes() *gin.Engine {
 	router := gin.Default()
 	router.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"*"},
+		AllowOrigins:     []string{"http://localhost:3000", "http://*", "https://*"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-		AllowHeaders:     []string{"Origin"},
+		AllowHeaders:     []string{"Origin", "Content-Length", "Content-Type", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
 		AllowCredentials: true,
 		AllowWildcard:    true,
