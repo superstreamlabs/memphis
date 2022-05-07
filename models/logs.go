62cd675780c7e57138be4e975659b63baee7764d
idanasulinStrech <idan@memphis.dev>
2022-05-07 17:42:23 +0300

    test broker retry

---
+++ models/logs.go
@@ -0,0 +1,35 @@
+// Copyright 2021-2022 The Memphis Authors
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package models
+
+import (
+	"bytes"
+	"encoding/json"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type Log struct {
+	ID           primitive.ObjectID `json:"id" bson:"_id"`
+	Log          string             `json:"log" bson:"log"`
+	Type         string             `json:"type" bson:"type"`
+	CreationDate time.Time          `json:"creation_date" bson:"creation_date"`
+}
+
+func (log Log) ToBytes() []byte {
+	bytesLog := new(bytes.Buffer)
+	json.NewEncoder(bytesLog).Encode(log)
+	return bytesLog.Bytes()
+}
