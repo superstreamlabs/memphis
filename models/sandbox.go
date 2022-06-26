2fb190b9155d7826138e199401e4620bc57471a5
idanasulinStrech <idan@memphis.dev>
2022-06-26 16:19:50 +0300

    add sandbox logic

--- models/sandbox.go
+++ models/sandbox.go
@@ -13,6 +13,25 @@
 
 package models
 
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type SandboxUser struct {
+	ID              primitive.ObjectID `json:"id" bson:"_id"`
+	Username        string             `json:"username" bson:"username"`
+	FirstName       string             `json:"first_name" bson:"first_name"`
+	LastName        string             `json:"last_name" bson:"last_name"`
+	Password        string             `json:"password" bson:"password"`
+	HubUsername     string             `json:"hub_username" bson:"hub_username"`
+	HubPassword     string             `json:"hub_password" bson:"hub_password"`
+	UserType        string             `json:"user_type" bson:"user_type"`
+	AlreadyLoggedIn bool               `json:"already_logged_in" bson:"already_logged_in"`
+	CreationDate    time.Time          `json:"creation_date" bson:"creation_date"`
+	AvatarId        int                `json:"avatar_id" bson:"avatar_id"`
+}
 type SandboxLoginSchema struct {
 	Google_token string `json: "google_token" binding:"required"`
 }
