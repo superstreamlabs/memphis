54a65afd3d49f971205e833f49c818b55cb19ac8
idanasulinStrech <idan@memphis.dev>
2022-07-18 15:16:34 +0300

    error message fix

--- handlers/stations.go
+++ handlers/stations.go
@@ -595,8 +595,8 @@ func (sh StationsHandler) GetMessageDetails(c *gin.Context) {
 	producedByHeader := natsMsg.Header.Get("producedBy")
 
 	if connectionIdHeader == "" || producedByHeader == "" {
-		logger.Error("Error while getting notified about a poison message: Missing mandatory message headers")
-		c.AbortWithStatusJSON(configuration.SHOWABLE_ERROR_STATUS_CODE, gin.H{"message": "Error while getting notified about a poison message: Missing mandatory message headers"})
+		logger.Error("Error while getting notified about a poison message: Missing mandatory message headers, please upgrade the SDk version you are using")
+		c.AbortWithStatusJSON(configuration.SHOWABLE_ERROR_STATUS_CODE, gin.H{"message": "Error while getting notified about a poison message: Missing mandatory message headers, please upgrade the SDk version you are using"})
 		return
 	}
 
