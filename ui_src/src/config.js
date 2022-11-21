2f18a3f79c92e55e320b951843cb65cb9cd35491
idanasulinStrech <idan@memphis.dev>
2022-11-21 14:02:33 +0200

    static files

--- ui_src/src/config.js
+++ ui_src/src/config.js
@@ -14,8 +14,10 @@
 
 const environment = process.env.NODE_ENV ? process.env.NODE_ENV : 'development';
 const SERVER_URL_PRODUCTION = `${window.location.href.split('//')[1].split('/')[0]}/api`;
+var WS_SERVER_URL_PRODUCTION = `${window.location.href.split('//')[1].split('/')[0]}`;
+if (WS_SERVER_URL_PRODUCTION.includes(":")) // for urls contain port
+    WS_SERVER_URL_PRODUCTION = WS_SERVER_URL_PRODUCTION.split(":")[0]
 const SSL_PREFIX = window.location.protocol === 'https:' ? 'https://' : 'http://';
-
 export const SERVER_URL = environment === 'production' ? `${SSL_PREFIX}${SERVER_URL_PRODUCTION}` : 'http://localhost:9000/api';
 
 export const HANDLE_REFRESH_INTERVAL = 600000;
@@ -23,8 +25,7 @@ export const SHOWABLE_ERROR_STATUS_CODE = 666;
 export const AUTHENTICATION_ERROR_STATUS_CODE = 401;
 export const DOC_URL = 'https://docs.memphis.dev/memphis/memphis/overview';
 export const PRIVACY_URL = 'https://app.gitbook.com/o/-MSyW3CRw3knM-KGk6G6/s/t7NJvDh5VSGZnmEsyR9h/memphis/privacy';
-export const SOCKET_URL = environment === 'production' ? `ws://${SERVER_URL_PRODUCTION}:7770` : 'ws://localhost:7770';
-
+export const SOCKET_URL = environment === 'production' ? `ws://${WS_SERVER_URL_PRODUCTION}:7770` : 'ws://localhost:7770';
 export const GOOGLE_CLIENT_ID = '916272522459-u0f4n2lh9llsielb3l5rob3dnt1fco76.apps.googleusercontent.com';
 export const REDIRECT_URI = environment === 'production' ? 'https://sandbox.memphis.dev/login' : 'http://localhost:9000/login';
 export const GITHUB_CLIENT_ID = environment === 'production' ? '4dc1b3238c4d7563e426' : '51b0330eb3b34bc8f641';
