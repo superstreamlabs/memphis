6d0588add7f6c19b0bd74aa60dbd89918b26340b
avrham <aneeman2010@gmail.com>
2022-12-07 17:30:36 +0200

    change header

--- ui_src/src/domain/signup/index.js
+++ ui_src/src/domain/signup/index.js
@@ -1,4 +1,3 @@
-// Credit for The NATS.IO Authors
 // Copyright 2021-2022 The Memphis Authors
 // Licensed under the Apache License, Version 2.0 (the “License”);
 // you may not use this file except in compliance with the License.
