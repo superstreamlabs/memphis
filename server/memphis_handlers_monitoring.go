23d702c5746490f5ca459320c8e6cdc32c9a01ef
Shay Bratslavsky <shay@memphis.dev>
2022-12-25 09:32:28 +0200

    handle poison messages for non native stations (#587)
    * handle poison messages for non native stations
    
    * add comments
    
    * fix message journey for station with .

--- server/memphis_handlers_monitoring.go
+++ server/memphis_handlers_monitoring.go
@@ -260,12 +260,15 @@ func (mh MonitoringHandler) GetStationOverviewData(c *gin.Context) {
 		c.AbortWithStatusJSON(500, gin.H{"message": "Server error"})
 		return
 	}
-
-	connectedCgs, disconnectedCgs, deletedCgs, err := consumersHandler.GetCgsByStation(stationName, station)
-	if err != nil {
-		serv.Errorf("GetStationOverviewData: At station " + body.StationName + ": " + err.Error())
-		c.AbortWithStatusJSON(500, gin.H{"message": "Server error"})
-		return
+	connectedCgs, disconnectedCgs, deletedCgs := make([]models.Cg, 0), make([]models.Cg, 0), make([]models.Cg, 0)
+	// Only native stations have CGs
+	if station.IsNative {
+		connectedCgs, disconnectedCgs, deletedCgs, err = consumersHandler.GetCgsByStation(stationName, station)
+		if err != nil {
+			serv.Errorf("GetStationOverviewData: At station " + body.StationName + ": " + err.Error())
+			c.AbortWithStatusJSON(500, gin.H{"message": "Server error"})
+			return
+		}
 	}
 
 	auditLogs, err := auditLogsHandler.GetAuditLogsByStation(station)
