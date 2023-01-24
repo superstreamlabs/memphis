f14f34ac46104e666b45383938906ac7b1559145
idanasulinStrech <idan@memphis.dev>
2023-01-24 17:40:44 +0200

    version update

--- server/const.go
+++ server/const.go
@@ -40,7 +40,7 @@ var (
 
 const (
 	// VERSION is the current version for the server.
-	VERSION = "0.4.3-beta"
+	VERSION = "0.4.4-beta"
 
 	// PROTO is the currently supported protocol.
 	// 0 was the original
