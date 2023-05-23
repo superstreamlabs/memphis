87a219b7dc3629e0711a1df2bf9e08b65eb1638c
svetaStrech <sveta@strech.io>
2023-05-23 11:51:24 +0300

    remove comments

--- ui_src/src/services/valueConvertor.js
+++ ui_src/src/services/valueConvertor.js
@@ -197,9 +197,6 @@ export const diffDate = (date) => {
 export const hex_to_ascii = (str1) => {
     var hex = str1.toString();
     var str = '';
-    // for (var n = 0; n < hex.length; n += 2) {
-    //     str += String.fromCharCode(parseInt(hex.substr(n, 2), 16));
-    // }
     str = decodeURIComponent(hex.replace(/[0-9a-f]{2}/g, '%$&'));
     return str;
 };
