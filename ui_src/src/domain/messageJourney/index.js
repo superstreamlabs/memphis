e335b4f56927006912a0803c8656b2f863fcfaae
shay23b <shay@memphis.dev>
2023-06-04 10:27:18 +0300

    remove space

--- ui_src/src/domain/messageJourney/index.js
+++ ui_src/src/domain/messageJourney/index.js
@@ -66,7 +66,7 @@ const MessageJourney = () => {
         const sc = StringCodec();
         try {
             (async () => {
-                const rawBrokerName = await state.socket?.request(`$memphis_ws_subs.poison_message_journey_data.${messageId}`,  sc.encode('SUB'));
+                const rawBrokerName = await state.socket?.request(`$memphis_ws_subs.poison_message_journey_data.${messageId}`, sc.encode('SUB'));
                 const brokerName = JSON.parse(sc.decode(rawBrokerName?._rdata))['name'];
                 sub = state.socket?.subscribe(`$memphis_ws_pubs.poison_message_journey_data.${messageId}.${brokerName}`);
             })();
