e285bf03d6a3d3c0aec912cbff69ce59d1737988
shohamroditimemphis <shoham@memphis.dev>
2023-06-06 11:59:27 +0300

    test

--- server/memphis_handlers_producers.go
+++ server/memphis_handlers_producers.go
@@ -189,52 +189,50 @@ func (s *Server) createProducerDirectV0(c *client, reply string, cpr createProdu
 	respondWithErr(s, reply, err)
 }
 
-func (s *Server) createProducerDirect() simplifiedMsgHandler {
-	return func(c *client, subject, reply string, msg []byte) {
-		serv.Warnf("createProducerDirect")
-		// var cpr createProducerRequestV1
-		var resp createProducerResponse
+func (s *Server) createProducerDirect(c *client, subject, reply string, msg []byte) simplifiedMsgHandler {
+	// return func(c *client, subject, reply string, msg []byte) {
+	serv.Warnf("createProducerDirect")
+	// var cpr createProducerRequestV1
+	var resp createProducerResponse
 
-		// if err := json.Unmarshal(msg, &cpr); err != nil || cpr.RequestVersion < 1 {
-		// 	var cprV0 createProducerRequestV0
-		// 	if err := json.Unmarshal(msg, &cprV0); err != nil {
-		// 		s.Errorf("createProducerDirect: %v", err.Error())
-		// 		respondWithRespErr(s, reply, err, &resp)
-		// 		return
-		// 	}
-		// 	s.createProducerDirectV0(c, reply, cprV0)
-		// 	return
-		// }
+	// if err := json.Unmarshal(msg, &cpr); err != nil || cpr.RequestVersion < 1 {
+	// 	var cprV0 createProducerRequestV0
+	// 	if err := json.Unmarshal(msg, &cprV0); err != nil {
+	// 		s.Errorf("createProducerDirect: %v", err.Error())
+	// 		respondWithRespErr(s, reply, err, &resp)
+	// 		return
+	// 	}
+	// 	s.createProducerDirectV0(c, reply, cprV0)
+	// 	return
+	// }
 
-		// sn, err := StationNameFromStr(cpr.StationName)
-		// if err != nil {
-		// 	s.Warnf("createProducerDirect: Producer " + cpr.Name + " at station " + cpr.StationName + ": " + err.Error())
-		// 	respondWithRespErr(s, reply, err, &resp)
-		// 	return
-		// }
+	// sn, err := StationNameFromStr(cpr.StationName)
+	// if err != nil {
+	// 	s.Warnf("createProducerDirect: Producer " + cpr.Name + " at station " + cpr.StationName + ": " + err.Error())
+	// 	respondWithRespErr(s, reply, err, &resp)
+	// 	return
+	// }
 
-		// clusterSendNotification, schemaVerseToDls, err := s.createProducerDirectCommon(c, cpr.Name, cpr.ProducerType, cpr.ConnectionId, sn)
-		// if err != nil {
-		// 	respondWithRespErr(s, reply, err, &resp)
-		// 	return
-		// }
+	// clusterSendNotification, schemaVerseToDls, err := s.createProducerDirectCommon(c, cpr.Name, cpr.ProducerType, cpr.ConnectionId, sn)
+	// if err != nil {
+	// 	respondWithRespErr(s, reply, err, &resp)
+	// 	return
+	// }
 
-		// resp.SchemaVerseToDls = schemaVerseToDls
-		// resp.ClusterSendNotification = clusterSendNotification
-		// schemaUpdate, err := getSchemaUpdateInitFromStation(sn)
-		// if err == ErrNoSchema {
-		// 	respondWithResp(s, reply, &resp)
-		// 	return
-		// }
-		// if err != nil {
-		// 	s.Errorf("createProducerDirect: Producer " + cpr.Name + " at station " + cpr.StationName + ": " + err.Error())
-		// 	respondWithRespErr(s, reply, err, &resp)
-		// 	return
-		// }
-
-		// resp.SchemaUpdate = *schemaUpdate
-		respondWithResp(s, reply, &resp)
-	}
+	// resp.SchemaVerseToDls = schemaVerseToDls
+	// resp.ClusterSendNotification = clusterSendNotification
+	// schemaUpdate, err := getSchemaUpdateInitFromStation(sn)
+	// if err == ErrNoSchema {
+	// 	respondWithResp(s, reply, &resp)
+	// 	return
+	// }
+	// if err != nil {
+	// 	s.Errorf("createProducerDirect: Producer " + cpr.Name + " at station " + cpr.StationName + ": " + err.Error())
+	// 	respondWithRespErr(s, reply, err, &resp)
+	// 	return
+	// }
+	respondWithResp(s, reply, &resp)
+	return nil
 }
 
 func (ph ProducersHandler) GetAllProducers(c *gin.Context) {
