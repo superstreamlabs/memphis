6aeb5b6d98a9a24a0418168cf4df3f913ce72200
daniel-davidd <daniel@ip-192-168-1-113.eu-central-1.compute.internal>
2023-06-12 21:57:25 +0300

    sdk schema works with json

--- server/memphis_handler_sdk_schema.go
+++ server/memphis_handler_sdk_schema.go
@@ -3,6 +3,7 @@ package server
 import (
 	"encoding/json"
 	"memphis/db"
+	"strconv"
 )
 
 type CreateSchemaReq struct {
@@ -10,6 +11,7 @@ type CreateSchemaReq struct {
 	Type              string `json:"type"`
 	CreatedByUsername string `json:"created_by_username"`
 	SchemaContent     string `json:"schema_content"`
+	MessageStructName string `json:"message_struct_name"`
 }
 
 func (s *Server) createSchemaDirect(c *client, reply string, msg []byte) {
@@ -33,10 +35,7 @@ func (s *Server) createSchemaDirect(c *client, reply string, msg []byte) {
 
 	if exist {
 		if existedSchema.Type == csr.Type {
-			err = updateSchemaVersion(existedSchema, csr)
-			if err != nil {
-				s.Errorf("createSchemaDirect: Schema " + csr.Name + ": " + err.Error())
-			}
+			s.updateSchemaVersion(existedSchema.ID, tenantName, csr)
 			return
 		} else {
 			s.Errorf("createSchemaDirect: Schema " + csr.Name + ": Bad Schema Type")
@@ -44,23 +43,66 @@ func (s *Server) createSchemaDirect(c *client, reply string, msg []byte) {
 		}
 	}
 
-	err = createNewSchemaDirect(csr, tenantName)
+	s.createNewSchemaDirect(csr, tenantName)
+
+}
+
+func (s *Server) updateSchemaVersion(schemaID int, tenantName string, newSchemaReq CreateSchemaReq) {
+	_, user, err := db.GetUserByUsername(newSchemaReq.CreatedByUsername, tenantName)
+
+	countVersions, err := db.GetShcemaVersionsCount(schemaID, user.TenantName)
 	if err != nil {
-		s.Errorf("createSchemaDirect: Schema " + csr.Name + ": " + err.Error())
+		s.Errorf("updateSchemaVersion: Schema " + newSchemaReq.Name + ": " + err.Error())
+		return
 	}
-	return
+
+	versionNumber := countVersions + 1
+
+	descriptor := ""
+	if newSchemaReq.Type == "protobuf" {
+		descriptor, err = generateSchemaDescriptor(newSchemaReq.Name, 1, newSchemaReq.SchemaContent, newSchemaReq.Type)
+		if err != nil {
+			s.Warnf("CreateNewSchemaDirectn: Schema " + newSchemaReq.Name + ": " + err.Error())
+			return
+		}
+	}
+
+	newSchemaVersion, rowsUpdated, err := db.InsertNewSchemaVersion(versionNumber, user.ID, user.Username, newSchemaReq.SchemaContent, schemaID, newSchemaReq.MessageStructName, descriptor, false, tenantName)
+	if err != nil {
+		s.Warnf("updateSchemaVersion: " + err.Error())
+		return
+	}
+	if rowsUpdated == 1 {
+		message := "Schema Version " + strconv.Itoa(newSchemaVersion.VersionNumber) + " has been created by " + user.Username
+		s.Noticef(message)
+	} else {
+		s.Warnf("CreateNewVersion: Schema " + newSchemaReq.Name + ": Version " + strconv.Itoa(newSchemaVersion.VersionNumber) + " already exists")
+		return
+	}
+
 }
 
-func updateSchemaVersion(existedSchema *Schema, newSchemaReq CreateSchemaReq) error {
-
-}
-
-func createNewSchemaDirect(newSchemaReq CreateSchemaReq, tenantName string) error {
+func (s *Server) createNewSchemaDirect(newSchemaReq CreateSchemaReq, tenantName string) {
 	schemaVersionNumber := 1
-	newSchema, rowUpdated, err = db.InsertNewSchema(newSchemaReq.Name, newSchemaReq.Type, newSchemaReq.CreatedByUsername, tenantName)
 
-	exist, user, err := GetUserByUsername(newSchemaReq.CreatedByUsername, tenantName)
+	_, user, err := db.GetUserByUsername(newSchemaReq.CreatedByUsername, tenantName)
+
+	descriptor := ""
+	if newSchemaReq.Type == "protobuf" {
+		descriptor, err = generateSchemaDescriptor(newSchemaReq.Name, 1, newSchemaReq.SchemaContent, newSchemaReq.Type)
+		if err != nil {
+			s.Warnf("CreateNewSchemaDirectn: Schema " + newSchemaReq.Name + ": " + err.Error())
+			return
+		}
+	}
+
+	newSchema, rowUpdated, err := db.InsertNewSchema(newSchemaReq.Name, newSchemaReq.Type, newSchemaReq.CreatedByUsername, tenantName)
+
 	if rowUpdated == 1 {
-		_, _, err := db.InsertNewSchemaVersion(schemaVersionNumber, user.ID, user.Username, newSchemaReq.SchemaContent, newSchema.ID, ///add missing fields)
+		_, _, err := db.InsertNewSchemaVersion(schemaVersionNumber, user.ID, user.Username, newSchemaReq.SchemaContent, newSchema.ID, newSchemaReq.MessageStructName, descriptor, true, tenantName)
+		if err != nil {
+			s.Errorf("createNewSchemaDirect: " + err.Error())
+			return
+		}
 	}
 }
