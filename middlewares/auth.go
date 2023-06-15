9694a2be6b34feb2c17cedc8b0a7ef935087586d
idanasulinStrech <idan@memphis.dev>
2023-06-15 06:44:40 +0300

    typo

--- middlewares/auth.go
+++ middlewares/auth.go
@@ -33,7 +33,7 @@ var noNeedAuthRoutes = []string{
 	"/api/status",
 	"/api/monitoring/getclusterinfo",
 	"/api/tenants/createtenant",
-	"/api/usermgmt/approvedinvitation",
+	"/api/usermgmt/approveinvitation",
 }
 
 var refreshTokenRoute string = "/api/usermgmt/refreshtoken"
