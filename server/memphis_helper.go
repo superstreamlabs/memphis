bbe2a5770a955df43a9108afb24614751683d958
shay23b <shay@memphis.dev>
2023-07-04 21:04:19 +0300

    fix validatePassword

--- server/memphis_helper.go
+++ server/memphis_helper.go
@@ -28,6 +28,7 @@ import (
 	"strconv"
 	"strings"
 	"time"
+	"unicode"
 
 	"github.com/gofrs/uuid"
 	"github.com/nats-io/nuid"
@@ -1454,14 +1455,37 @@ func (s *Server) MoveResourcesFromOldToNewDefaultAcc() error {
 }
 
 func validatePassword(password string) error {
-	pattern := `^([A-Z]*)([a-z]*)(.[!?\-@#$%])[A-Za-z\d!?\-@#$%]{8,}$`
-	regex, err := regexp.Compile(pattern)
-	if err != nil {
-		return err
-	}
-	if regex.MatchString(password) {
-		return nil
-	} else {
+	pattern := `^[A-Za-z0-9!?\-@#$%]+$`
+	match, _ := regexp.MatchString(pattern, password)
+	if !match {
 		return errors.New("Invalid Password")
 	}
+	if len(password) < 8 {
+		return errors.New("Invalid Password")
+	}
+	var (
+		hasUppercase   bool
+		hasLowercase   bool
+		hasDigit       bool
+		hasSpecialChar bool
+	)
+
+	for _, char := range password {
+		switch {
+		case unicode.IsUpper(char):
+			hasUppercase = true
+		case unicode.IsLower(char):
+			hasLowercase = true
+		case unicode.IsDigit(char):
+			hasDigit = true
+		case char == '!' || char == '?' || char == '-' || char == '@' || char == '#' || char == '$' || char == '%':
+			hasSpecialChar = true
+		}
+	}
+
+	if hasUppercase && hasLowercase && hasDigit && hasSpecialChar {
+		return nil
+	}
+
+	return errors.New("Invalid Password")
 }
