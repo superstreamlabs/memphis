9cb9d08af75a9f646c671aea548b36d6d2bdaa0b
idanasulinStrech <idan@memphis.dev>
2023-08-06 11:14:13 +0300

    produce endpoint - bugfix

--- server/memphis_cloud.go
+++ server/memphis_cloud.go
@@ -2087,10 +2087,11 @@ func (sh StationsHandler) Produce(c *gin.Context) {
 		return
 	}
 
-	if body.MsgHdrs != nil {
-		body.MsgHdrs["$memphis_producedBy"] = "UI"
-		body.MsgHdrs["$memphis_connectionId"] = "UI"
+	if body.MsgHdrs == nil {
+		body.MsgHdrs = make(map[string]string)
 	}
+	body.MsgHdrs["$memphis_producedBy"] = "UI"
+	body.MsgHdrs["$memphis_connectionId"] = "UI"
 	// if shouldRoundRobin {
 	// 	partition := (i % len(station.Partitions)) + 1
 	// 	subject = fmt.Sprintf("%s$%v.final", stationName.Intern(), partition)
