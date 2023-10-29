49ff9664be357795951d14ba11dd46497aebec5a
svetaStrech <sveta@strech.io>
2023-10-29 19:49:41 +0200

    .

--- ui_src/src/domain/administration/softwareUpdates/index.js
+++ ui_src/src/domain/administration/softwareUpdates/index.js
@@ -13,21 +13,103 @@
 import './style.scss';
 
 import React, { useContext, useState, useEffect } from 'react';
+import Button from '../../../components/button';
+import { ReactComponent as LogoTexeMemphis } from '../../../assets/images/logoTexeMemphis.svg';
+import { ReactComponent as RedirectIcon } from '../../../assets/images/redirectIcon.svg';
+import { ReactComponent as RedirectWhiteIcon } from '../../../assets/images/exportWhite.svg';
+import HalfDoughnutChart from './components/halfDoughnutChart';
+const data = {
+    labels: ['Red', 'Green', 'Blue'],
+    datasets: [
+        {
+            data: [40, 60, 100],
+            backgroundColor: ['red', 'green', 'transparent'],
+            hoverBackgroundColor: ['red', 'green', 'transparent'],
+            borderWidth: 2, // Add a border for the outer doughnut
+            borderColor: 'white', // Set the border color for the outer doughnut
+            cutoutPercentage: 70, // Adjust the cutout percentage to control the size of the inner doughnut
+            borderAlign: 'inner', // Make sure the border is drawn inside the doughnut
+            // hoverBorderWidth: 5 // Adjust the hover border width
+            hoverBorderWidth: 0, // Adjust the hover border width
+            borderRadius: 10,
+            hoverBorderColor: 'transparent'
+        }
+    ]
+};
 
+const options = {
+    cutoutPercentage: 70, // Adjust the cutout percentage to control the size of the innermost hole
+    rotation: Math.PI,
+    tooltips: false
+};
+
+const options2 = {
+    cutoutPercentage: 10, // Adjust the cutout percentage to control the size of the innermost hole
+    rotation: Math.PI,
+    tooltips: false
+};
 function SoftwareUpates({}) {
     return (
         <div className="softwate-updates-container">
             <div className="rows">
-                <div className="item-component"></div>
+                <div className="item-component">
+                    <div className="title-component">
+                        <div className="versions">
+                            <LogoTexeMemphis alt="Memphis logo" />
+                            <label className="curr-version">v0.4.3 - Beta</label>
+                            <span className="new-version">
+                                <label>New Version available </label>
+                                <RedirectIcon alt="redirect" />
+                            </span>
+                        </div>
+                        <Button
+                            width="200px"
+                            height="36px"
+                            placeholder={
+                                <span className="">
+                                    <label>View Change log </label>
+                                    <RedirectWhiteIcon alt="redirect" />
+                                </span>
+                            }
+                            colorType="white"
+                            radiusType="circle"
+                            backgroundColorType={'purple'}
+                            fontSize="12px"
+                            fontFamily="InterSemiBold"
+                            // htmlType="submit"
+                            onClick={() => console.log('clicked')}
+                        />
+                    </div>
+                </div>
                 <div className="statistics">
-                    <div className="item-component"></div>
-                    <div className="item-component"></div>
-                    <div className="item-component"></div>
-                    <div className="item-component"></div>
+                    <div className="item-component wrapper">
+                        <label className="title">Amount of brokers</label>
+                        <label className="numbers">600</label>
+                    </div>
+                    <div className="item-component wrapper">
+                        <label className="title">Amount of brokers</label>
+                        <label className="numbers">600</label>
+                    </div>
+                    <div className="item-component wrapper">
+                        <label className="title">Amount of brokers</label>
+                        <label className="numbers">600</label>
+                    </div>
+                    <div className="item-component wrapper">
+                        <label className="title">Amount of brokers</label>
+                        <label className="numbers">600</label>
+                    </div>
                 </div>
                 <div className="charts">
-                    <div className="item-component"></div>
-                    <div className="item-component"></div>
+                    <div className="item-component">
+                        <label className="title">Amount of brokers</label>
+                        <HalfDoughnutChart />
+                    </div>
+                    <div className="item-component">
+                        <label className="title">Amount of brokers</label>
+                    </div>
+                    <div className="item-component">
+                        <label className="title">Amount of brokers</label>
+                    </div>
                 </div>
             </div>
         </div>
