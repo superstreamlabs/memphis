7b98e91935651120b115247b7d761061850d173a
shohamroditimemphis <shoham@memphis.dev>
2023-11-23 16:07:14 +0200

    fix

--- server/memphis_handlers_user_mgmt.go
+++ server/memphis_handlers_user_mgmt.go
@@ -162,10 +162,16 @@ func removeTenantResources(tenantName string, user models.User) error {
 	}
 	// TODO: send response of DeleteAndGetAttachedFunctionsByStation to microservice to delete
 
+	err = db.RemoveStationsByTenant(tenantName)
+	if err != nil {
+		return err
+	}
+
 	err = sendDeleteAllFunctionsReqToMS(user, tenantName, "github", "", "", "aws_lambda", "", true)
 	if err != nil {
 		return err
 	}
+
 	err = deleteInstallationForAuthenticatedGithubApp(user.TenantName)
 	if err != nil {
 		return err
@@ -189,11 +195,6 @@ func removeTenantResources(tenantName string, user models.User) error {
 	}
 
 	err = db.DeleteAllSharedLocks(tenantName)
-	if err != nil {
-		return err
-	}
-
-	err = db.RemoveStationsByTenant(tenantName)
 	if err != nil {
 		return err
 	}
