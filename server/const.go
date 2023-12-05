c7f3536daac3c44aaaca806a810a8371e1798fcf
idanasulinStrech <idan@memphis.dev>
2023-12-05 11:46:58 +0200

    version update

--- server/const.go
+++ server/const.go
@@ -41,7 +41,7 @@ var (
 
 const (
 	// VERSION is the current version for the memphis.
-	VERSION = "1.4.0"
+	VERSION = "1.4.1"
 
 	DEFAULT_SERVER_NAME                        = "memphis-0"
 	DEFAULT_WS_PORT                            = 7770
