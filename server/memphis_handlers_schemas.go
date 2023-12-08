14c11673599323e6526f186453f6eb1e60edb6c8
shohamroditimemphis <shoham@memphis.dev>
2023-12-08 11:22:04 +0200

    enable create version to existing schema via sdk

--- server/memphis_handlers_schemas.go
+++ server/memphis_handlers_schemas.go
@@ -940,18 +940,6 @@ func (s *Server) updateSchemaVersion(schemaID int, tenantName string, newSchemaR
 		return err
 	}
 
-	_, currentSchema, err := db.GetSchemaVersionByNumberAndID(countVersions, schemaID)
-	if err != nil {
-		s.Errorf("[tenant: %v][user: %v]updateSchemaVersion at db.GetSchemaVersionByNumberAndID: Schema %v: %v", tenantName, user.Username, newSchemaReq.Name, err.Error())
-		return err
-	}
-
-	if currentSchema.SchemaContent == newSchemaReq.SchemaContent {
-		alreadyExistInDB := fmt.Sprintf("%v already exist in the db", newSchemaReq.Name)
-		s.Warnf("[tenant: %v][user: %v]at updateSchemaVersion, %v already exist in the db", tenantName, user.Username, newSchemaReq.Name)
-		return errors.New(alreadyExistInDB)
-	}
-
 	versionNumber := countVersions + 1
 
 	descriptor := ""
